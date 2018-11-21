d28baf0230429ab2e667836a3847a735bf978be5
Gordon Force <gordon.force@perficient.com>
2018-11-21 11:02:54 -0800

    Initial commit

---
+++ src/main/java/org/superbiz/moviefun/podcasts/PodcastController.java
@@ -0,0 +1,22 @@
+package org.superbiz.moviefun.podcasts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class PodcastController {
+    private PodcastRepository podcastRepository;
+
+    public PodcastController(PodcastRepository podcastRepository) {
+        this.podcastRepository = podcastRepository;
+    }
+
+    @GetMapping("/podcasts")
+    public String allPodcasts(Map<String, Object> model) {
+        model.put("podcasts", podcastRepository.findAll() );
+        return "podcasts";
+    }
+
+}
