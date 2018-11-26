d8f1444a976ebaae81f4d7ff6f3a7caacded9e65
Gordon Force <gordon.force@perficient.com>
2018-11-26 09:55:09 -0800

    Altered the build to create separate jar files for the movies and podcast components

--- components/movies/src/main/java/org/superbiz/moviefun/movies/ActionServlet.java
+++ components/movies/src/main/java/org/superbiz/moviefun/movies/ActionServlet.java
@@ -14,16 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.moviefun;
+package org.superbiz.moviefun.movies;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
-import org.superbiz.moviefun.movies.Movie;
-import org.superbiz.moviefun.movies.MoviesBean;
 
-import javax.ejb.EJB;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -40,8 +37,11 @@ public class ActionServlet extends HttpServlet {
 
     public static int PAGE_SIZE = 5;
 
-    @EJB
     private MoviesBean moviesBean;
+
+    public ActionServlet(MoviesBean moviesBean) {
+        this.moviesBean = moviesBean;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
