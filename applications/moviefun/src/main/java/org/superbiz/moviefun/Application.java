9e72bb21eb78cc33558fb3e6f5135efe4937f131
Gordon Force <gordon.force@perficient.com>
2019-01-09 13:11:41 -0800

    Incorporated service discovery.

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -1,9 +1,10 @@
 package org.superbiz.moviefun;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -11,14 +12,9 @@ import org.superbiz.moviefun.moviesui.ActionServlet;
 import org.superbiz.moviefun.moviesui.MovieClient;
 import org.superbiz.moviefun.podcastsUI.PodcastClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
-
-    @Value("${movies.ms.url}")
-    private String moviesURL;
-
-    @Value("${podcasts.ms.url}")
-    private String podcastsURL;
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
@@ -29,6 +25,7 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
@@ -36,12 +33,12 @@ public class Application {
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
-        return new MovieClient(moviesURL, restOperations);
+        return new MovieClient("//movies-ms/movies", restOperations);
     }
 
     @Bean
     public PodcastClient podcastClient(RestOperations restOperations) {
-        return new PodcastClient(podcastsURL, restOperations);
+        return new PodcastClient("//podcasts-ms/podcasts", restOperations);
     }
 
 }
--- applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/MovieMicroserviceApplication.java
+++ applications/movies-ms/src/main/java/org/superbiz/moviefun/movies/MovieMicroserviceApplication.java
@@ -2,8 +2,10 @@ package org.superbiz.moviefun.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieMicroserviceApplication {
 
