8492a06dc6773b4972c3e2f6a06286bf8b002501
Gordon Force <gordon.force@perficient.com>
2019-01-22 14:31:30 -0800

    Added a circuit breaker to the podcast client lookup

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -3,6 +3,7 @@ package org.superbiz.moviefun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.superbiz.moviefun.moviesui.MovieClient;
 import org.superbiz.moviefun.podcastsUI.PodcastClient;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
 
